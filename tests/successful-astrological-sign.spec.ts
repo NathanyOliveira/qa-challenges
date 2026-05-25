f2eee63c3cd05baa1d1420e2ff4329e68c210c2a
Nathany Oliveira Maciel <naty_om_3@hotmail.com>
2026-05-25 21:16:23 +0200

    Test to validate Invalid date format

--- tests/successful-astrological-sign.spec.ts
+++ tests/successful-astrological-sign.spec.ts
@@ -3,14 +3,19 @@ import { expect, test } from '@playwright/test';
 test.beforeEach(async({page}) => {
     await page.goto('http://localhost:5173/challenge-1')
     await expect(page).toHaveTitle('QA Challenges')
-    
 })
 
-
-
 test("Successfully find astrological sign", async({page}) => {
-    await page.getByPlaceholder("Enter your name").fill('batata')
-    await page.getByPlaceholder("dd-mm-yyyy").fill('29-12-1990')
+    await page.getByPlaceholder("Enter your name").fill('Xuxa')
+    await page.getByPlaceholder("dd-mm-yyyy").fill('27-03-1963')
     await page.getByRole('button', { name: 'Find Astrological Sign' }).click()
-
+    await expect(page.getByText("Xuxa, your astrological sign is Aries")).toBeVisible()
 }) 
+
+test('Invalid date format', async({page}) =>{
+    await page.getByPlaceholder("Enter your name").fill('Xuxa')
+    await page.getByPlaceholder("dd-mm-yyyy").fill('27/03-1963')
+    await page.getByRole('button', { name: 'Find Astrological Sign' }).click()
+    await expect(page.getByText("Invalid date. Please use the format dd-mm-yyyy.")).toBeVisible()
+
+})
