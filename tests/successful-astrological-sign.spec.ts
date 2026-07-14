a5a23d7f69b96dd55bc99cbe5ec597a6f8a7cb86
Nathany <naty_om_3@hotmail.com>
2026-07-14 17:42:29 +0200

    add button state validation to challenge 1

--- tests/successful-astrological-sign.spec.ts
+++ tests/successful-astrological-sign.spec.ts
@@ -1,5 +1,7 @@
 import { expect, test } from '@playwright/test';
 
+test.describe('Challlenge 1 - Astrological Sign', () =>{
+
 test.beforeEach(async({page}) => {
     await page.goto('http://localhost:5173/challenge-1')
     await expect(page).toHaveTitle('QA Challenges')
@@ -17,5 +19,18 @@ test('Invalid date format', async({page}) =>{
     await page.getByLabel("birthday").fill('27/03-1963')
     await page.getByRole('button', { name: 'Find Astrological Sign' }).click()
     await expect(page.getByText("Invalid date. Please use the format dd-mm-yyyy.")).toBeVisible()
+})
+
+test('Button is disabled until both name and birthday are entered', async({page}) => {
+   const button = page.getByRole('button', {
+    name:  'Find Astrological Sign'
+    })
+    await expect(button).toBeDisabled();
+    await page.getByLabel('name').fill('Xuxa');
+    await expect(button).toBeDisabled();
+    await page.getByLabel('birthday').fill('27-03-1963');
+    await expect(button).toBeEnabled();
+
+})
 
 })
